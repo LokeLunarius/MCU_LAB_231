71dec6fb3e56a5399b31c123c599533664282b4b
LokeLunarius <loc.bui_akira343@hcmut.edu.vn>
2023-11-27 12:23:10 +0700

    27/11/2023

---
+++ Lab_4/Lab4_STM32/Core/Inc/global.h
@@ -0,0 +1,21 @@
+/*
+ * global.h
+ *
+ *  Created on: Nov 23, 2023
+ *      Author: Loke
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#include "main.h"
+#include "software_timer.h"
+#include "stdio.h"
+#include "fsm_uart.h"
+#include "string.h"
+
+extern ADC_HandleTypeDef hadc1;
+
+extern UART_HandleTypeDef huart2;
+
+#endif /* INC_GLOBAL_H_ */
